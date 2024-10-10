f0e5db118801e44edab8bdb5c4d8fb51d53209b2
cliecy <guanjiezou@gmail.com>
2024-10-10 20:38:42 +0900

    change the 路由

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout() {
         <Stack.Screen name="loginpage/Register" options={{ headerShown: false }} />
         <Stack.Screen name="userpage/UserPage" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-        <Stack.Screen name="mypage/AdminControl" options={{ headerShown: true }} />
+        <Stack.Screen name="adminpage/AdminControl" options={{ headerShown: true }} />
       </Stack>
     </ThemeProvider>
   );
